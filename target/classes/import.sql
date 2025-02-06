523b72e524158f5a3b9763a74e0fd2ed03c02575
Joao Marcos <joaomarcos11.tech@gmail.com>
2025-02-05 23:33:09 -0300

    removendo target

--- target/classes/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO videos (id, status, dataCriacao, dataAtualizacao, idUsuario) VALUES
-('23e52205-4d9d-41e6-a7f3-271af4f5316b', 2, Now(), Now(), 'exemplo@exemplo.com');
-
-INSERT INTO videos (id, status, dataCriacao, dataAtualizacao, idUsuario) VALUES
-('4e1bb65c-b3c0-4229-964b-c10241b7aca4', 3, Now(), Now(), 'exemplo@exemplo.com');
