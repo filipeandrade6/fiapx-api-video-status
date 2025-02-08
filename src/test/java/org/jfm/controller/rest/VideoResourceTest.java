028f7719a92fc7905607fb7dc66ec892a629cd49
Joao Marcos <joaomarcos11.tech@gmail.com>
2025-02-08 14:34:45 -0300

    teste

--- src/test/java/org/jfm/controller/rest/VideoResourceTest.java
+++ src/test/java/org/jfm/controller/rest/VideoResourceTest.java
@@ -1,62 +1,68 @@
-// package org.jfm.controller.rest;
+package org.jfm.controller.rest;
 
-// import java.util.List;
-// import java.util.UUID;
+import java.util.List;
+import java.util.UUID;
 
-// import org.jfm.domain.entities.Video;
-// import org.jfm.domain.usecases.VideoUseCase;
-// import org.jfm.factory.VideoFactory;
-// import org.jfm.infra.repository.adaptersql.mapper.VideoMapper;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.mockito.InjectMocks;
-// import org.mockito.Mock;
-// import org.mockito.Mockito;
-// import org.mockito.MockitoAnnotations;
+import org.jfm.domain.entities.Video;
+import org.jfm.domain.usecases.VideoUseCase;
+import org.jfm.factory.VideoFactory;
+import org.jfm.infra.repository.adaptersql.mapper.VideoMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
-// import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-// import io.quarkus.test.junit.QuarkusTest;
-// import io.restassured.http.ContentType;
-// import jakarta.inject.Inject;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import jakarta.inject.Inject;
 
-// import static io.restassured.RestAssured.given;
-// import static org.hamcrest.CoreMatchers.is;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
-// @QuarkusTest
-// public class VideoResourceTest {
+@QuarkusTest
+public class VideoResourceTest {
 
-//   @Inject
-//   VideoResource videoResource;
+  @Inject
+  VideoResource videoResource;
 
-//   @InjectMocks
-//   VideoResource videoResourceMock;
+  @InjectMocks
+  VideoResource videoResourceMock;
 
-//   @Mock
-//   VideoUseCase videoUseCase;
+  @Mock
+  VideoUseCase videoUseCase;
 
-//   @Mock
-//   VideoMapper videoMapper;
+  @Mock
+  VideoMapper videoMapper;
 
-//   @BeforeEach
-//   public void setup() {
-//     MockitoAnnotations.openMocks(this);
-//   }
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.openMocks(this);
+  }
 
-//   @Test
-//   public void buscarPorIdTest() throws Exception {
-//     // ObjectMapper mapper = new ObjectMapper();
+  // BDD
+  // Feature: Buscar um video específico pelo ID
+  // Cenário: Buscar video por Id com sucesso
+  @Test
+  public void buscarPorIdTest() throws Exception {
 
-//     Video video = VideoFactory.montar();
+    // Dado video com ID "23e52205-4d9d-41e6-a7f3-271af4f5316b" que existe no sistema
+    UUID idVideo = UUID.fromString("23e52205-4d9d-41e6-a7f3-271af4f5316b");
+    Video video = VideoFactory.montar();
 
-//     Mockito.when(videoUseCase.buscarPorId(UUID.fromString("23e52205-4d9d-41e6-a7f3-271af4f5316b")))
-//       .thenReturn(video);
+    Mockito.when(videoUseCase.buscarPorId(idVideo))
+      .thenReturn(video);
 
-//     given()
-//       .contentType(ContentType.JSON)
-//       .when().pathParam("id", video.getId())
-//       .get("/videos/{id}")
-//       .then()
-//       .statusCode(200);
-//   }
-// }
+    // Quando fizer requisição GET para "videos/{id}" com ID igual a "23e52205-4d9d-41e6-a7f3-271af4f5316b"
+    // Então, a resposta tem status 200
+    given()
+      .contentType(ContentType.JSON)
+      .when().pathParam("id", video.getId())
+      .get("/videos/{id}")
+      .then()
+      .statusCode(200);
+  }
+}
